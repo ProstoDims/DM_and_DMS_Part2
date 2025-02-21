3b2457f7498ec0706a5ed8404279725272eb2cf1
ProstoDims <davoker@mail.ru>
2025-02-21 16:17:02 +0300

    Add triggers from task2

--- Lab2/Lab2.sql
+++ Lab2/Lab2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE GROUPS (
     ID NUMBER PRIMARY KEY,
-    NAME VARCHAR2(100) UNIQUE,
+    NAME VARCHAR2(100),
     C_VAL NUMBER DEFAULT 0
 );
 
@@ -8,5 +8,62 @@ CREATE TABLE STUDENTS (
     ID NUMBER PRIMARY KEY,
     NAME VARCHAR2(100),
     GROUP_ID NUMBER,
-    CONSTRAINT fk_group FOREIGN KEY (GROUP_ID) REFERENCES GROUPS(ID) ON DELETE CASCADE
+    CONSTRAINT fk_group FOREIGN KEY (GROUP_ID) REFERENCES GROUPS(ID)
 );
+
+CREATE SEQUENCE group_seq START WITH 1 INCREMENT BY 1 NOCACHE;
+CREATE SEQUENCE students_seq START WITH 1 INCREMENT BY 1 NOCACHE;
+
+CREATE OR REPLACE TRIGGER groups_before_insert
+BEFORE INSERT ON GROUPS
+FOR EACH ROW
+DECLARE
+    v_count_name INTEGER;
+BEGIN
+
+    SELECT COUNT(*)
+    INTO v_count_name
+    FROM GROUPS
+    WHERE NAME = :NEW.NAME;
+
+    IF v_count_name > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Group name already exists');
+    END IF;
+
+    IF :NEW.ID IS NULL THEN
+        SELECT group_seq.NEXTVAL INTO :NEW.ID FROM DUAL;
+    ELSE
+        DECLARE
+            v_count NUMBER;
+        BEGIN
+            SELECT COUNT(*) INTO v_count FROM GROUPS WHERE ID=:NEW.ID;
+            IF v_count > 0 THEN
+                SELECT group_seq.NEXTVAL INTO :NEW.ID FROM DUAL;
+            ELSE
+                NULL;
+            END IF;
+        END;
+    END IF;
+
+
+END;
+
+CREATE OR REPLACE TRIGGER students_before_insert
+BEFORE INSERT ON STUDENTS
+FOR EACH ROW
+BEGIN
+    IF :NEW.ID IS NULL THEN
+        SELECT students_seq.NEXTVAL INTO :NEW.ID FROM DUAL;
+    ELSE
+        DECLARE
+            v_count NUMBER;
+        BEGIN
+            SELECT COUNT(*) INTO v_count FROM STUDENTS WHERE ID=:NEW.ID;
+            IF v_count > 0 THEN
+                SELECT students_seq.NEXTVAL INTO :NEW.ID FROM DUAL;
+            ELSE
+                NULL;
+            END IF;
+        END;
+    END IF;
+END;
